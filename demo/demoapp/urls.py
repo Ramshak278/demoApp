d84a27837579eb48abd81a43a9c87ec8292f4976
Ramshak278 <ramshak278@gmail.com>
2024-04-01 17:32:07 +0500

    updated urls

--- demo/demoapp/urls.py
+++ demo/demoapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
+from books.API.views import BookByAuthorView, BookByTitleView, BookByUserIdView
 from .Api.views import Registrations, MyTokenObtainPairView, UserRetrieveUpdateAPIView
 
 urlpatterns=[
     path("register",Registrations.as_view()),
     path("login",MyTokenObtainPairView.as_view()),
     path("verify",UserRetrieveUpdateAPIView.as_view())
+    path('books/by_author/<str:author_name>/', BookByAuthorView.as_view()),
+    path('books/by_title/<str:book_name>/', BookByTitleView.as_view()),
+    path('books/by_user/<int:user_id>/', BookByUserIdView.as_view()),
 ]
