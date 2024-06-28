9ae1a461c292dc67999d3e76d9fe67d2ff56108b
Ramshak278 <ramshak278@gmail.com>
2024-06-28 16:43:11 +0500

    email smtp settings

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -200,3 +200,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     },
 # }
 
+
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'  # Update with your email host
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'ramshak278@gmail.com'
+# EMAIL_HOST_PASSWORD = ''
+
+
+DEFAULT_FROM_EMAIL = 'contact@confiatech.com'
+SERVER_EMAIL = 'contact@confiatech.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'mail.confiatech.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'contact@confiatech.com'
+EMAIL_HOST_PASSWORD = 'Confia@123'
