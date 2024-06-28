79a353354496da8eecb05bc351882cea9f001642
Ramshak278 <ramshak278@gmail.com>
2024-06-28 16:16:44 +0500

    update email

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -200,11 +200,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     },
 # }
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.confiatech.com'  # Update with your email host
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "contact@confiatech.com"
-EMAIL_HOST_PASSWORD = "Confia@123"
-
